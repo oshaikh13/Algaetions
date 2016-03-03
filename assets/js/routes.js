6e2ad408b97b7b08d72f6a349401065ffc9446be
Omar Shaikh <oshaikh13@gmail.com>
2016-03-03 21:36:52 +0300

    Another large update

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -6,5 +6,13 @@ var foundationRoutes =
     "animationIn":"hingeInFromMiddleY bounceInOut",
     "animationOut":"zoomOut bounceInOut",
     "path":"templates/home.html"
+  },
+
+  {
+    "name":"mapsettings",
+    "url":"/settings",
+    "animationIn":"hingeInFromMiddleY bounceInOut",
+    "animationOut":"zoomOut bounceInOut",
+    "path":"templates/settings.html"
   }
 ]; 
